f10c2ce7a860b1033da585df931f57de610dfb45
kushmittal05 <70550154+kushmittal05@users.noreply.github.com>
2021-10-28 16:55:47 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+ import firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCmYPSPvd_JCMaRkpaHjwBEJk1SOzOVY78",
+  authDomain: "project-60-8012d.firebaseapp.com",
+  projectId: "project-60-8012d",
+  storageBucket: "project-60-8012d.appspot.com",
+  messagingSenderId: "279628696459",
+  appId: "1:279628696459:web:696d682f46f745086ffd8e"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+  export default firebase.database()
+ 
+
+  
